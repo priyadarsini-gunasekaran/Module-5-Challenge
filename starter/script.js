f3a0f57c1e01187e3f44ab02a5681bfb17bd79fe
tm28823289 <77493239+tm28823289@users.noreply.github.com>
2023-01-09 15:47:48 +0000

    getRandom commit

--- starter/script.js
+++ starter/script.js
@@ -208,8 +208,9 @@ function getPasswordOptions() {
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  return arr[Math.floor(Math.random() * arr.length)];
 }
+
 
 // Function to generate password with user input
 function generatePassword() {
